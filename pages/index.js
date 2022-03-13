b0626a94f143baf2e7962dd471db2353a2947f7d
zollywoods <zollywooods@gmail.com>
2022-03-13 15:15:06 -0400

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <Head>
         <title>Everybody</title>
         <meta  content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="Everybody" key="title" />
+        <meta property="og:title" content="Everybodys" key="title" />
       </Head>
             <div className={styles.backgroundWrapper}>  
               {/* <Image
